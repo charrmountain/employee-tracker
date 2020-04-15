a7b24d5525209de1281a9f78d83acea855ea9062
Charlotte Mountain <charr.mt@gmail.com>
2020-04-15 08:15:58 -0700

    add employee

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
-const cTable = require('console.table');
+const cTable = require("console.table");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -24,6 +24,9 @@ connection.connect((err) => {
   start();
 });
 
+const sqlString =
+  "SELECT employee.id, employee.first_name, employee.last_name, role.title AS position, role.salary AS salary, department.name AS department, CONCAT(manager.first_name,' ', manager.last_name) AS manager FROM employee LEFT JOIN employee AS manager ON employee.manager_id = manager.id LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id";
+
 function start() {
   inquirer
     .prompt([
@@ -35,6 +38,7 @@ function start() {
           "View all Employees",
           "View Employees by Roles",
           "View Employees by Department",
+          "Add an Employee",
           "Exit",
         ],
       },
@@ -50,6 +54,9 @@ function start() {
       if (answers.options === "View Employees by Department") {
         view_by_department();
       }
+      if (answers.options === "Add an Employee") {
+        add_employee();
+      }
       if (answers.options === "Exit") {
         connection.end();
       }
@@ -59,17 +66,16 @@ function start() {
       process.exit(1);
     });
 }
-
+// -----------------------------VIEWS-----------------------------
+// -----------------------------VIEW ALL-----------------------------
 //view ALL employees
 function view_employees() {
-  const sqlString =
-    "SELECT employee.id, employee.first_name, employee.last_name, role.title AS position, role.salary AS salary, department.name AS department, CONCAT(manager.first_name,' ', manager.last_name) AS manager FROM employee LEFT JOIN employee AS manager ON employee.manager_id = manager.id LEFT JOIN role ON role.id = employee.role_id LEFT JOIN department ON department.id = role.department_id;";
   // the callback will receive an error object or response object from connection.query
   const callback = (err, res) => {
     if (err) {
       throw err;
     }
-    console.table(res)
+    console.table(res);
     console.log("-----------------------------------");
     return start();
   };
@@ -77,6 +83,7 @@ function view_employees() {
   return connection.query(sqlString, callback);
 }
 
+// -----------------------------VIEW BY ROLES-----------------------------
 //view ALL roles
 function view_by_roles() {
   inquirer
@@ -109,34 +116,256 @@ function view_by_roles() {
 }
 
 function view_CEO() {
-  const sqlString = "";
+  const CEO = " WHERE role.title = 'CEO'";
   // the callback will receive an error object or response object from connection.query
   const callback = (err, res) => {
     if (err) {
       throw err;
     }
-    res.forEach((row) =>
-      console.log(`${row.id} | ${row.title} | ${row.salary}`)
-    );
+    console.table(res);
     console.log("-----------------------------------");
     return start();
   };
   // pass an sql query string and callback function
-  return connection.query(sqlString, callback);
+  return connection.query(sqlString + CEO, callback);
 }
 
-//view ALL departments
-function view_by_department() {
-  const sqlString = "SELECT * FROM department";
+function view_engineer() {
+  const Engineer = " WHERE role.title = 'Engineer'";
   // the callback will receive an error object or response object from connection.query
   const callback = (err, res) => {
     if (err) {
       throw err;
     }
-    res.forEach((row) => console.log(`${row.id} | ${row.name}`));
+    console.table(res);
     console.log("-----------------------------------");
     return start();
   };
   // pass an sql query string and callback function
-  return connection.query(sqlString, callback);
+  return connection.query(sqlString + Engineer, callback);
 }
+
+function view_tech() {
+  const Tech = " WHERE role.title = 'Tech Support'";
+  // the callback will receive an error object or response object from connection.query
+  const callback = (err, res) => {
+    if (err) {
+      throw err;
+    }
+    console.table(res);
+    console.log("-----------------------------------");
+    return start();
+  };
+  // pass an sql query string and callback function
+  return connection.query(sqlString + Tech, callback);
+}
+
+function view_secretary() {
+  const Secretary = " WHERE role.title = 'Secretary'";
+  // the callback will receive an error object or response object from connection.query
+  const callback = (err, res) => {
+    if (err) {
+      throw err;
+    }
+    console.table(res);
+    console.log("-----------------------------------");
+    return start();
+  };
+  // pass an sql query string and callback function
+  return connection.query(sqlString + Secretary, callback);
+}
+
+// -----------------------------VIEW BY DEPARTMENT-----------------------------
+//view ALL department
+function view_by_department() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What role would you like to search?",
+          name: "options",
+          choices: ["Executive", "Technology", "Communications"],
+        },
+      ])
+      .then((answers) => {
+        // If place item
+        if (answers.options === "Executive") {
+          view_executive();
+        }
+        if (answers.options === "Technology") {
+          view_technology();
+        }
+        if (answers.options === "Communications") {
+          view_communications();
+        }
+        if (answers.options === "Back to Main Menu") {
+          start();
+        }
+      });
+  }
+
+  function view_executive() {
+    const executive = " WHERE department.name = 'Executive'";
+    // the callback will receive an error object or response object from connection.query
+    const callback = (err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.table(res);
+      console.log("-----------------------------------");
+      return start();
+    };
+    // pass an sql query string and callback function
+    return connection.query(sqlString + executive, callback);
+  }
+  
+  function view_technology() {
+    const tech_dept = " WHERE department.name = 'Technology'";
+    // the callback will receive an error object or response object from connection.query
+    const callback = (err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.table(res);
+      console.log("-----------------------------------");
+      return start();
+    };
+    // pass an sql query string and callback function
+    return connection.query(sqlString + tech_dept, callback);
+  }
+  
+  function view_communications() {
+    const communications = " WHERE department.name = 'Communications'";
+    // the callback will receive an error object or response object from connection.query
+    const callback = (err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.table(res);
+      console.log("-----------------------------------");
+      return start();
+    };
+    // pass an sql query string and callback function
+    return connection.query(sqlString + communications, callback);
+  }
+
+
+// -----------------------------ADD-----------------------------
+function view_by_roles() {
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "What role would you like to search?",
+          name: "options",
+          choices: ["CEO", "Engineer", "Tech Support", "Secretary"],
+        },
+      ])
+      .then((answers) => {
+        // If place item
+        if (answers.options === "CEO") {
+          view_CEO();
+        }
+        if (answers.options === "Engineer") {
+          view_engineer();
+        }
+        if (answers.options === "Tech Support") {
+          view_tech();
+        }
+        if (answers.options === "Secretary") {
+          view_secretary();
+        }
+        if (answers.options === "Back to Main Menu") {
+          start();
+        }
+      });
+  }
+  
+  function view_CEO() {
+    const CEO = " WHERE role.title = 'CEO'";
+    // the callback will receive an error object or response object from connection.query
+    const callback = (err, res) => {
+      if (err) {
+        throw err;
+      }
+      console.table(res);
+      console.log("-----------------------------------");
+      return start();
+    };
+    // pass an sql query string and callback function
+    return connection.query(sqlString + CEO, callback);
+  }
+  
+
+//   --------------------------ADD-----------------------------------
+
+function add_employee() {
+    const query_role = `SELECT id, title FROM role`;
+    const query_employees = `SELECT id, first_name, last_name FROM employee`;
+    const insert_query = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+    
+    const employee_values = [];
+    
+    connection.query(query_role, (err, res) => {
+      if (err) {
+        throw err;
+      }
+      const role_array = res.map((row) => `${row.id} ${row.title}`);
+      
+      connection.query(query_employees, (err, res) => {
+        if (err) {
+          throw err;
+        }
+        const manager_array = res.map(
+          (row) => `${row.id} ${row.first_name} ${row.last_name}`
+        );
+        
+        manager_array.push("null");
+        return inquirer
+          .prompt([
+            {
+              name: "first",
+              type: "input",
+              message: "What is the employee's first name?",
+            },
+            {
+              name: "last",
+              type: "input",
+              message: "What is the employee's last name?",
+            },
+            {
+              name: "role",
+              type: "rawlist",
+              choices: role_array,
+              message: "Choose a role for the employee",
+            },
+            {
+              name: "manager",
+              type: "rawlist",
+              choices: manager_array,
+              message: "Choose a manager for the employee",
+            },
+          ])
+          .then((answer) => {
+            const role_ID = answer.role.split(" ");
+            const manager_ID = answer.manager.split(" ");
+
+            employee_values.push(answer.first, answer.last, role_ID[0], manager_ID[0]);
+            
+            connection.query(
+              insert_query,
+              [employee_values[0], employee_values[1], employee_values[2], employee_values[3]],
+              (err, res) => {
+                if (err) {
+                  throw err;
+                }
+              }
+            );
+
+            console.log("Employee added");
+            start();
+          });
+      });
+    });
+  }
+  
--- apps.js
+++ apps.js
@@ -1,29 +1,63 @@
-// function to handle view all employees by MANAGER
-function view_by_manager() {
-    return inquirer
-      .prompt([
-        {
-          name: "manager",
-          type: "input",
-          message: "Under which manager?",
-        },
-      ])
-      .then((answer) => {
-        const sqlString =
-          "SELECT * , employee.last_name, employee.role_id, manager.first_name AS manager_first FROM employee JOIN employee AS manager ON employee.manager_id = manager.id WHERE manager.first_name = ?";
-        const manager_answer = [answer.manager];
-        const callback = (err, res) => {
-          if (err) {
-            throw err;
-          }
-          res.forEach((row) =>
-            console.log(`${row.first_name} | ${row.last_name}`)
+function add_employee() {
+  const query_employees = `SELECT id, first_name, last_name FROM employee`;
+  const insert_query = `INSERT INTO employee(first_name, last_name, role_id, manager_id) VALUES (?, ?, ?, ?)`;
+  const query_role = `SELECT id, title FROM role`;
+  const values = [];
+  connection.query(query_role, (err, res) => {
+    if (err) {
+      throw err;
+    }
+    const role_array = res.map((row) => `${row.id} ${row.title}`);
+    connection.query(query_employees, (err, res) => {
+      if (err) {
+        throw err;
+      }
+      const emp_array = res.map(
+        (row) => `${row.id} ${row.first_name} ${row.last_name}`
+      );
+      empArr.push("null");
+      return inquirer
+        .prompt([
+          {
+            name: "first",
+            type: "input",
+            message: "What is the employee's first name?",
+          },
+          {
+            name: "last",
+            type: "input",
+            message: "What is the employee's last name?",
+          },
+          {
+            name: "role",
+            type: "rawlist",
+            choices: role_array,
+            message: "Choose a role for the employee",
+          },
+          {
+            name: "manager",
+            type: "rawlist",
+            choices: emp_array,
+            message: "Choose a manager for the employee",
+          },
+        ])
+        .then((answer) => {
+          const roleID = answer.role.split(" ");
+          const managerID = answer.manager.split(" ");
+          values.push(answer.first, answer.last, roleID[0], managerID[0]);
+          console.log(values);
+          connection.query(
+            insert_query,
+            [values[0], values[1], values[2], values[3]],
+            (err, res) => {
+              if (err) {
+                throw err;
+              }
+            }
           );
-          console.log("-----------------------------------");
-          return start();
-        };
-        // pass an sql query string and callback function
-        return connection.query(sqlString, manager_answer, callback);
-      });
-  }
-  
+          console.log("Employee added");
+          start();
+        });
+    });
+  });
+}
